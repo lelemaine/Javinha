8a19a1785b77d5a0780c3e9e77cd04bb59da76f6
lelemaine <l.ms@outlook.com.br>
2021-04-12 09:23:36 -0300

    Create Exercício1.java
    Exercício 1

---
+++ Exercício1.java
@@ -0,0 +1,27 @@
+public class Exercício1 {
+
+
+
+    public static void main(String[] args) {
+       Double C, K, Re, Ra, F;
+
+       C = 5.66;
+
+        F = C*1.8+32;
+        K = C + 273.15;
+        Re = C * 0.8;
+        Ra = C * 1.8 + 32 + 459.67;
+
+        System.out.println("Temperatura em Fahrenheit: " + F);
+        System.out.println("Temperatura em Kelvin: " + K);
+        System.out.println("Temperatura em Reamur: " + Re);
+        System.out.println("Temperatura em Rankine: " + Ra);
+
+
+
+
+
+
+
+    }
+}
