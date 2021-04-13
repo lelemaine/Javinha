a8c21443bb8eb6c388ae3c9387caa6dc7abed821
lelemaine <l.ms@outlook.com.br>
2021-04-13 20:46:21 -0300

    Create Exercício5.java

---
+++ src/Exercício5.java
@@ -0,0 +1,14 @@
+public class Exercício5 {
+
+    public static void main(String[] args) {
+        double m, h, t, cavalim;
+
+        m = 3047.0;
+        h = 256.55;
+        t = 0.54;
+        cavalim = (m * h / t) / 745.6999;
+
+        System.out.println("Quantidade de cavalos é = " + cavalim);
+
+    }
+}
